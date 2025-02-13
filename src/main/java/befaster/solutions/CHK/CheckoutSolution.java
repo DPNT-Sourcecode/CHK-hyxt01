b2404f6588f923f6e9235571a60780054f0dce58
sbx_user1051 <sbx_user1051@169.254.30.141>
2025-02-13 00:23:42 +0000

    Thu Feb 13 00:23:42 UTC 2025

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,60 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.Map;
+
 public class CheckoutSolution {
+
+    private static final Integer INVALID_SKU = -1;
+    private static final String VALID_SKU_REGEX = "[A-D]*";
+
+    private final Map<String, Integer> skuPriceMap;
+    private final Map<String, Integer> skuDiscountMap;
+
+    public CheckoutSolution() {
+        skuPriceMap = Map.of(
+            "A", 50,
+            "B", 30,
+            "C", 20,
+            "D", 15
+        );
+
+        skuDiscountMap = Map.of(
+            "A", 130,
+            "B", 45
+        );
+    }
+
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        boolean isInvalid = skus == null || skus.isEmpty() || !skus.matches(VALID_SKU_REGEX);
+        if (isInvalid) {
+            return INVALID_SKU;
+        }
+
+
+    }
+
+    private class Discount {
+        private final String sku;
+        private final Integer quantity;
+        private final Integer price;
+
+        public Discount(String sku, Integer quantity, Integer price) {
+            this.sku = sku;
+            this.quantity = quantity;
+            this.price = price;
+        }
+
+        public String getSku() {
+            return sku;
+        }
+
+        public Integer getQuantity() {
+            return quantity;
+        }
+
+        public Integer getPrice() {
+            return price;
+        }
     }
 }

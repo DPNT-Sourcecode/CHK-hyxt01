e401e6fc858c89630e20aa9cf557c77f6efe80d0
sbx_user1051 <sbx_user1051@169.254.20.193>
2025-02-13 12:32:33 +0000

    Thu Feb 13 12:32:33 UTC 2025

--- src/main/java/befaster/solutions/CHK/GroupDiscountManager.java
+++ src/main/java/befaster/solutions/CHK/GroupDiscountManager.java
@@ -1,5 +1,6 @@
 package befaster.solutions.CHK;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -24,16 +25,18 @@ public class GroupDiscountManager {
 
     private void applyGroupDiscounts(Map<Character, Integer> skuCountMap) {
         for (Discount groupDiscount : groupDiscountList) {
+            List<Character> skuInGroup = new ArrayList<>();
             int skuGroupCount = 0;
             for (char sku : groupDiscount.getSkuList()) {
                 skuGroupCount += skuCountMap.getOrDefault(sku, 0);
+                skuInGroup.add(sku);
             }
 
             Optional<Integer> valueWithDiscount = groupDiscount.apply(skuGroupCount);
             if (valueWithDiscount.isPresent()) {
                 groupedDiscountValue += valueWithDiscount.get();
 
-                for (char sku : groupDiscount.getSkuList()) {
+                for (char sku : skuInGroup) {
                     int remainingCount = skuCountMap.getOrDefault(sku, 0) - 1;
                     if (remainingCount >= 0) this.nonApplicableSkus.put(sku, remainingCount);
                 }
@@ -41,3 +44,4 @@ public class GroupDiscountManager {
         }
     }
 }
+

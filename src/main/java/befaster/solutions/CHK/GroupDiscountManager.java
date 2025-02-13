15ce8a9230640a4449b5a4c104849761398e5881
sbx_user1051 <sbx_user1051@169.254.30.141>
2025-02-13 04:30:38 +0000

    Thu Feb 13 04:30:38 UTC 2025

--- src/main/java/befaster/solutions/CHK/GroupDiscountManager.java
+++ src/main/java/befaster/solutions/CHK/GroupDiscountManager.java
@@ -1,35 +1,44 @@
 package befaster.solutions.CHK;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class GroupDiscountManager {
 
-    List<Discount> discountList = List.of(
+    List<Discount> groupDiscountList = List.of(
             new Discount(List.of('S', 'T', 'X', 'Y', 'Z'), 3, 45)
     );
 
-    public int value;
-    public List<Character> remainingSkuList;
+    public int count = 0;
+    public Map<Character, Integer> remaining;
+    public int checkoutValue;
 
-    public int apply(String skuList) {
-//        remainingSkuList = List.of(skuList.toCharArray());
-//
-//        for (Discount discount : discountList) {
-//            int skuGroupCount = 0;
-//            for (char sku : skuList.toCharArray()) {
-//                if (discount.isApplicable(sku)) {
-//                    skuGroupCount++;
-//                    remainingSkuList.remove(sku);
-//                }
-//            }
-//            Optional<Integer> valueWithDiscount = discount.apply(skuGroupCount);
-//            if (valueWithDiscount.isPresent()) {
-//                value += valueWithDiscount.get();
-//            } else {
-//                remainingSkuList = skuList;
-//            }
-//        }
-        return -1;
+    public GroupDiscountManager(Map<Character, Integer> skuCountMap) {
+        applyGroupDiscounts(skuCountMap);
+    }
+
+    public List<Character> getNonApplicableSkus() {
+        return remaining;
+    }
+
+    private void applyGroupDiscounts(Map<Character, Integer> skuCountMap) {
+        int value = 0;
+        for (Discount groupDiscount : groupDiscountList) {
+            int skuGroupCount = 0;
+            for (char sku : groupDiscount.getSkuList()) {
+                skuGroupCount += skuCountMap.getOrDefault(sku, 0);
+            }
+
+            Optional<Integer> valueWithDiscount = groupDiscount.apply(skuGroupCount);
+            if (valueWithDiscount.isPresent()) {
+                value += valueWithDiscount.get();
+                for (char sku : groupDiscount.getSkuList()) {
+                    remaining.put(sku, skuCountMap.getOrDefault(sku, 0) - skuGroupCount);
+                }
+            }
+        }
+        return value;
     }
 }
+

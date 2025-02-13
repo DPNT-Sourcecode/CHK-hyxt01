505d8bbe767ac99570ff6fc517cf7b4935575237
sbx_user1051 <sbx_user1051@169.254.30.141>
2025-02-13 04:00:38 +0000

    Thu Feb 13 04:00:38 UTC 2025

--- src/main/java/befaster/solutions/CHK/GroupDiscountManager.java
+++ src/main/java/befaster/solutions/CHK/GroupDiscountManager.java
@@ -9,19 +9,26 @@ public class GroupDiscountManager {
             new Discount(List.of('S', 'T', 'X', 'Y', 'Z'), 3, 45)
     );
 
-    public int apply(List<Character> list) {
+    public int value;
+    public List<Character> remainingItems;
+
+    public int apply(List<Character> skuList) {
+        List<Character> remainingItems = skuList;
+
         for (Discount discount : discountList) {
             int skuGroupCount = 0;
             for (char sku : list) {
                 if (discount.isApplicable(sku)) {
                     skuGroupCount++;
+                    remainingItems.remove(sku);
                 }
             }
             Optional<Integer> valueWithDiscount = discount.apply(skuGroupCount);
             if (valueWithDiscount.isPresent()) {
-                return valueWithDiscount.get();
+                value += valueWithDiscount.get();
             }
         }
         return -1;
     }
 }
+

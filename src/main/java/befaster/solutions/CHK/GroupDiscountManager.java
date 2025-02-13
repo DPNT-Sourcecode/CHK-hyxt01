696d8fbd416e9527a669962a1b213dd8e894e2b7
sbx_user1051 <sbx_user1051@169.254.20.193>
2025-02-13 12:50:33 +0000

    Thu Feb 13 12:50:33 UTC 2025

--- src/main/java/befaster/solutions/CHK/GroupDiscountManager.java
+++ src/main/java/befaster/solutions/CHK/GroupDiscountManager.java
@@ -1,59 +1,66 @@
 package befaster.solutions.CHK;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class GroupDiscountManager {
 
-    List<Discount> groupDiscountList = List.of(
-            new Discount(List.of('S', 'T', 'X', 'Y', 'Z'), 3, 45)
-    );
+    private static final Set<Character> groupDiscountItems = Set.of('S', 'T', 'X', 'Y', 'Z');
+
 
     public Map<Character, Integer> nonApplicableSkus;
     public int groupedDiscountValue = 0;
 
     public GroupDiscountManager(Map<Character, Integer> skuCountMap) {
         this.nonApplicableSkus = skuCountMap;
-        applyGroupDiscounts(skuCountMap);
+        apply(skuCountMap);
     }
 
     Map<Character, Integer> getNonApplicableSkus() {
         return nonApplicableSkus;
     }
-
-    private void applyGroupDiscounts(Map<Character, Integer> skuCountMap) {
-        for (Discount groupDiscount : groupDiscountList) {
-            List<Character> skuInGroup = new ArrayList<>();
-            int skuGroupCount = 0;
-            for (char sku : groupDiscount.getSkuList()) {
-                int skuItemGroup = skuCountMap.getOrDefault(sku, 0);
-                if (skuItemGroup == 0) continue;
-
-                skuGroupCount += skuItemGroup;
-                for (int i = 0; i < skuItemGroup; i++) {
-                    skuInGroup.add(sku);
-                }
-            }
-
-            Optional<Integer> valueWithDiscount = groupDiscount.apply(skuGroupCount);
-            if (valueWithDiscount.isPresent()) {
-                groupedDiscountValue += valueWithDiscount.get();
-
-                for (char sku : skuInGroup) {
-                    int remainingCount = skuCountMap.getOrDefault(sku, 0) - 1;
-                    if (remainingCount >= 0) this.nonApplicableSkus.put(sku, remainingCount);
-                }
-            }
-        }
-    }
+//
+//    private void applyGroupDiscounts(Map<Character, Integer> skuCountMap) {
+//        for (Discount groupDiscount : groupDiscountList) {
+//            List<Character> skuInGroup = new ArrayList<>();
+//            int skuGroupCount = 0;
+//            for (char sku : groupDiscount.getSkuList()) {
+//                int skuItemGroup = skuCountMap.getOrDefault(sku, 0);
+//                if (skuItemGroup == 0) continue;
+//
+//                skuGroupCount += skuItemGroup;
+//                for (int i = 0; i < skuItemGroup; i++) {
+//                    skuInGroup.add(sku);
+//                }
+//            }
+//
+//            Optional<Integer> valueWithDiscount = groupDiscount.apply(skuGroupCount);
+//            if (valueWithDiscount.isPresent()) {
+//                groupedDiscountValue += valueWithDiscount.get();
+//
+//                for (char sku : skuInGroup) {
+//                    int remainingCount = skuCountMap.getOrDefault(sku, 0) - 1;
+//                    if (remainingCount >= 0) this.nonApplicableSkus.put(sku, remainingCount);
+//                }
+//            }
+//        }
+//    }
 
     private void apply(Map<Character, Integer> skuCountMap) {
+        List<Character> itemsInGroup = getGroupItems(skuCountMap);
+        int groupCount = itemsInGroup.size();
+    }
+
+    private List<Character> getGroupItems(Map<Character, Integer> skuCountMap) {
         List<Character> skuInGroup = new ArrayList<>();
-        for (char item : groupDiscountList.get)
+        for (char item : groupDiscountItems) {
+            for (int i = 0; i < skuCountMap.getOrDefault(item, 0); i++) {
+                skuInGroup.add(item);
+            }
+        }
+        return skuInGroup;
     }
 }
+
 
 
 

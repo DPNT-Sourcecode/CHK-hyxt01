92aac6e5e6b2f0d33aeb2c26185751f6e073da2f
sbx_user1051 <sbx_user1051@169.254.30.141>
2025-02-13 00:44:26 +0000

    Thu Feb 13 00:44:26 UTC 2025

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,12 +1,10 @@
 package befaster.solutions.CHK;
 
-import befaster.solutions.HLO.HelloSolution;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CheckoutSolutionTest {
     private CheckoutSolution checkoutSolution;
@@ -24,7 +22,7 @@ class CheckoutSolutionTest {
     }
 
     @Test
-    public void testSingleItems() {
+    public void testBasicPrices() {
         assertThat(checkoutSolution.checkout("A"), equalTo(50));
         assertThat(checkoutSolution.checkout("B"), equalTo(30));
         assertThat(checkoutSolution.checkout("C"), equalTo(20));
@@ -32,21 +30,12 @@ class CheckoutSolutionTest {
     }
 
     @Test
-    public void testMultipleItems() {
-        assertThat(checkoutSolution.checkout("ABCD"), equalTo(115));
-        assertThat(checkoutSolution.checkout("AAABBB"), equalTo(160));
-        assertThat(checkoutSolution.checkout("AAABBBD"), equalTo(175));
-    }
-
-    @Test
     public void testDiscounts() {
+        assertThat(checkoutSolution.checkout("AB"), equalTo(80));
         assertThat(checkoutSolution.checkout("AAA"), equalTo(130));
+        assertThat(checkoutSolution.checkout("AA"), equalTo(100));
         assertThat(checkoutSolution.checkout("AAAA"), equalTo(180));
-        assertThat(checkoutSolution.checkout("AAAAA"), equalTo(230));
-        assertThat(checkoutSolution.checkout("AAAAAA"), equalTo(260));
-
-        assertThat(checkoutSolution.checkout("BB"), equalTo(45));
-        assertThat(checkoutSolution.checkout("BBB"), equalTo(75));
         assertThat(checkoutSolution.checkout("BBBB"), equalTo(90));
+        assertThat(checkoutSolution.checkout("BBB"), equalTo(75));
     }
 }
